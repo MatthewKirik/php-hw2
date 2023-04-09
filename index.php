61835e8cd2b4add5294430c1e36acdb332f90bd5
Ivan Baturkin <ivanmysterys@gmail.com>
2023-04-09 17:32:57 +0300

    feat: add simple index page

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// Start a session to keep track of the user's login status
+session_start();
+
+// Check if the user is logged in
+if (isset($_SESSION['loggedin'])) {
+    // If the user is logged in, show the index page
+    echo "<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Index</title>
+        <!-- Include the styles.css file to style the page -->
+        <link rel='stylesheet' href='styles.css'>
+    </head>
+    <body>
+        <div class='container'>
+            <!-- Display a greeting message -->
+            <h2>Welcome to the index page!</h2>
+        </div>
+    </body>
+    </html>";
+} else {
+    // If the user is not logged in, redirect to the login page
+    header('Location: login.php');
+    exit;
+}
+?>
