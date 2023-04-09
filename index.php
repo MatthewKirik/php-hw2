c6980130d94e40d172ab429dd84937403877fefc
Ivan Baturkin <ivanmysterys@gmail.com>
2023-04-09 18:31:03 +0300

    feat: add logout button

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php
-// Start a session to keep track of the user's login status
-session_start();
 
-// Check if the user is logged in
-if (isset($_SESSION['loggedin'])) {
-    echo "<!DOCTYPE html>
-    <html>
-    <head>
-        <title>Index</title>
-        <link rel='stylesheet' href='styles.css'>
-    </head>
-    <body>
-        <div class='container'>
-            <h2>Welcome to the index page!</h2>
-        </div>
-    </body>
-    </html>";
-} else {
-    // If the user is not logged in, redirect to the login page
+session_start(['cookie_httponly' => true, 'cookie_secure' => true]);
+
+// If the user is not logged in, redirect to the login page
+if (!isset($_SESSION['loggedin'])) {
     header('Location: login.php');
     exit;
 }
+
+echo "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <title>Index</title>
+    <link rel='stylesheet' href='styles.css'>
+</head>
+<body>
+    <div class='index-container'>
+        <h2>Welcome to the index page!</h2>
+        <form action='logout.php' method='post'>
+            <input type='submit' name='logout' value='Logout' class='btn btn-primary'>
+        </form>
+    </div>
+</body>
+</html>";
+
 ?>
